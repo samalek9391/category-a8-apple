e87f3616ed7857742f274cd904e9a7cdc61596f3
samalek9391 <sa.malek9391@gmail.com>
2026-05-25 19:29:34 +0600

    footer is added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/shared/NavBar";
+import Footer from "@/components/shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({ children }) {
         <main>
         {children}
         </main>
-
+        <footer>
+          <Footer/>
+        </footer>
       </body>
     </html>
   );
