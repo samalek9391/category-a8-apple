5011566947bc9fec6041575d9ae4ab44f2c47587
samalek9391 <sa.malek9391@gmail.com>
2026-05-29 22:31:13 +0600

    sign in page is added

---
+++ src/app/auth/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h2>Sign up here..</h2>
+        </div>
+    );
+};
+
+export default RegisterPage;
